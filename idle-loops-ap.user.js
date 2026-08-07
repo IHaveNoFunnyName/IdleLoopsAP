947f97354a96c6ac44b522d07c7e7d38742551c6
Neffy <ihavenofunnyname@gmail.com>
2026-08-07 13:56:42 +1000

    Feat: Release 0.4.2

--- idle-loops-ap.user.js
+++ idle-loops-ap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Idle Loops AP
 // @namespace    http://tampermonkey.net/
-// @version      0.4.1
+// @version      0.4.2
 // @description  Mod for Idle Loops to integrate with AP
 // @author       Neffy
 // @include      https://lloyd-delacroix.github.io/omsi-loops/
@@ -11,10 +11,9 @@
 // ==/UserScript==
 function loadScript(id, src) {
 	const script = document.createElement('script');
-	const version = '0.4.1';
 	script.id = id;
-	script.src = `${src}-${version}.js`;
+	script.src = `${src}.js`;
 	script.setAttribute('crossorigin', 'anonymous');
 	document.head.appendChild(script);
 }
-setTimeout(() => loadScript('IdleLoopsAP', 'https://IHaveNoFunnyName.github.io/IdleLoopsAP/' + 'idle-loops-ap'), 200);
+setTimeout(() => loadScript('IdleLoopsAP', 'https://IHaveNoFunnyName.github.io/IdleLoopsAP/idle-loops-ap'), 200);
